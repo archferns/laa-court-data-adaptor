e5460ea5cbcb399c03e61cac40edcff8ba36eaa1
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-12-16 13:20:58 +0000

    Add fixture and spec for cracked_ineffective_trial model

--- app/models/cracked_ineffective_trial.rb
+++ app/models/cracked_ineffective_trial.rb
@@ -1,14 +1,5 @@
 # frozen_string_literal: true
 
-# example from Common platform hearing endpoint
-#
-# "crackedIneffectiveTrial": {
-#   "code": "A",
-#   "description": "On the date of trial the defendant, for the first time, enters a guilty plea which the prosecution accepts.",
-#   "id": "c4ca4238-a0b9-3382-8dcc-509a6f75849b",
-#   "type": "Cracked"
-# }
-
 class CrackedIneffectiveTrial
   include ActiveModel::Model
 
@@ -27,6 +18,6 @@ class CrackedIneffectiveTrial
   end
 
   def type
-    body["type"]
+    body["type"]&.downcase
   end
 end
