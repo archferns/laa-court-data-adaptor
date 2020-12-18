2f09c164467da74d76c44855e4f86d5669248e50
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-12-18 14:16:02 +0000

    Fixup cracked trial spec cop fixes

--- spec/models/cracked_ineffective_trial_spec.rb
+++ spec/models/cracked_ineffective_trial_spec.rb
@@ -7,27 +7,27 @@ RSpec.describe CrackedIneffectiveTrial, type: :model do
 
   subject(:cracked_ineffective_trial) { described_class.new(body: cracked_ineffective_trial_hash) }
 
-  describe '#id' do
+  describe "#id" do
     subject { cracked_ineffective_trial.id }
 
     it { is_expected.to eql("c4ca4238-a0b9-3382-8dcc-509a6f75849b") }
   end
 
-  describe '#type' do
+  describe "#type" do
     subject { cracked_ineffective_trial.type }
 
-    it 'is expected to downcase the type' do
+    it "is expected to downcase the type" do
       is_expected.to eql("cracked")
     end
   end
 
-  describe '#code' do
+  describe "#code" do
     subject { cracked_ineffective_trial.code }
 
     it { is_expected.to eql("A") }
   end
 
-  describe '#description' do
+  describe "#description" do
     subject { cracked_ineffective_trial.description }
 
     it { is_expected.to eql("On the date of trial the defendant, for the first time, enters a guilty plea which the prosecution accepts.") }
